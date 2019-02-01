0aa6c792b05a92da83504310eab6daff8c03e7c9
Adam <adam.schachterle@gmail.com>
2019-02-01 09:51:35 -0500

    added a few styles to the design

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use strict";
+$(document).ready(function () {
+    $(".power").click(function(){
+        $(".screen").toggleClass("clicked");
+    });
+});
+
+
+
+//     let clicked = true
+//     $(".power").click(function() {
+//         console.log("clicked")
+//         $(".screen").css("background", "url(http://www.sprinklesandbooze.com/wp/wp-content/uploads/2012/11/pete-and-pete.jpeg)");
+//         let clicked = false;
+//         console.log(clicked);
+//     })
+//     if ($(window).width() < 820 && clicked == false) {
+//         $(".power").click(function() {
+//         console.log('working')
+//         $(".screen").css("background-color", "#000000");
+//         })
+
+//     // let image = clicked ? "rgb(83, 104, 46)": "url(http://www.sprinklesandbooze.com/wp/wp-content/uploads/2012/11/pete-and-pete.jpeg)";
+//     // $(".screen").css(image);
+//     // clicked = !clicked;
+//     // console.log('hello')
+// }
