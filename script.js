b87fd393ac5c15a416c9d2f442b66671a2098ec0
Samantha Cayla Bajis <Sammi.Bajis@gmail.com>
2019-02-02 14:28:57 -0500

    added game functionality for javascript

--- script.js
+++ script.js
@@ -1,27 +1,80 @@
 "use strict";
 $(document).ready(function () {
-    $(".power").click(function(){
-        $(".screen").toggleClass("clicked");
+    var timer = null;
+    var time = 0;
+    $(`.power`).click(function(){
+        console.log('hello!')
+        $(`.screen`).toggleClass(`unclicked`);
+          time = 120;
+          showTimer();
+            // if $(“.screen”).toggleClass(“clicked”)
+          clearInterval(timer);
+        //   **** Adam clearInterval needs to be above timer =setInterval(...)
+        $(".cards").css("opacity", "1");
+          timer = setInterval(showTimer, 1000);
+        });
+        let matches = 0;                                // number of matches between the cards starts at 0
+
+    const randomize = () => {                   // where the cards are "shuffled" through the ul and li elements in the HTML
+        let deck = $('ul');                     // going through the desk of cards
+        let cards = $('ul li').length;          // going through each individual card
+  
+        for (let i = 1; i < cards; i++) {           
+        let a = Math.floor(Math.random() * cards); // i starts at 1 and will increment adding a new individual, random card to the deck (appending its index to the array)
+        $('li', deck).eq(a).appendTo(deck); 
+        } 
+      }
+
+      randomize();                              // randomize function is called
+      let showCards = [];
+      let clickedCard;
+      
+        
+      $(document.body).on("click", "li.card", function(event) {         // This function happens in the document (HTMLs) body and attaches the on click event to each listed card 
+  
+    
+        $(event.target).toggleClass("face-down show open");             // the event (which is the click) is targeted by the toggle class function to flip over the card starting from the face-down class
+        clickedCard = $(event.target).attr("class");                    // the clicked card is set equal to the on click event and uses the .attr method to return it to the FIRST matched class attribute
+        showCards.push(clickedCard);                                    // adding the clicked cards to the end of the array(show cards) when clicked and flipped back over
+       
+        if (showCards.length === 2) {                                   // just compares the length of the two cards clicked making sure it is just two cards before comparison is run
+          
+          const compare = () => {                                       // the compare function is declared
+            if (showCards[0] === showCards[1]) {                                  
+            $(".show").css({'background':''}).animate({ opacity: 0 },{duration: 800});    // if the showCards array equals the showCards clicked(so just one card) then show the second background from css, call the animate set in css to flip and lower the opactiy to 0 over the duration specified
+            // $(".show").toggleClass("show blank open");                                   // they toggled instead between a black card and the cards they needed to replace
+            showCards = [];
+            matches++;                                                                  // the showCards array will reset and the matches will increment by 1 pair
+            checkWin();
+          } else {   
+            $('.show').toggleClass("face-down show open");
+
+            showCards = [];
+          }
+      } 
+  
+
+        setTimeout(compare, 800);           // run compare function here
+        } 
+
+        const checkWin = () => {
+           if (showCards.length === 0) {
+            $('.container').html('<h1>You win!</h1>');
+           }
+        }
     });
-});
+        
+    function showTimer() {
+          if (time < 0) {
+            clearInterval(timer);
+                return;
+          }
+          function pad(value) {
+                return (value < 10 ? `0` : ``) + value;
+          }
+          $(`.timer`).text(Math.floor(time / 60) + `:` + pad(time % 60));
+          time--;
+        }
+ })
 
-
-
-//     let clicked = true
-//     $(".power").click(function() {
-//         console.log("clicked")
-//         $(".screen").css("background", "url(http://www.sprinklesandbooze.com/wp/wp-content/uploads/2012/11/pete-and-pete.jpeg)");
-//         let clicked = false;
-//         console.log(clicked);
-//     })
-//     if ($(window).width() < 820 && clicked == false) {
-//         $(".power").click(function() {
-//         console.log('working')
-//         $(".screen").css("background-color", "#000000");
-//         })
-
-//     // let image = clicked ? "rgb(83, 104, 46)": "url(http://www.sprinklesandbooze.com/wp/wp-content/uploads/2012/11/pete-and-pete.jpeg)";
-//     // $(".screen").css(image);
-//     // clicked = !clicked;
-//     // console.log('hello')
-// }
+ 
