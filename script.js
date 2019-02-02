4f8524ea5570b92ff87c0c9c6639bd95524d29f9
Adam <adam.schachterle@gmail.com>
2019-02-02 14:30:16 -0500

    additional work on project

--- script.js
+++ script.js
@@ -1,27 +1,142 @@
 "use strict";
 $(document).ready(function () {
-    $(".power").click(function(){
-        $(".screen").toggleClass("clicked");
-    });
-});
+    var timer = null;
+    var time = 0;
+    $(`.power`).click(function(){
+        $(`.screen`).toggleClass(`unclicked`);
+          time = 120;
+          showTimer();
+            // if $(“.screen”).toggleClass(“clicked”)
+          clearInterval(timer);
+        //   **** Adam clearInterval needs to be above timer =setInterval(...)
+        $(".cards").css("opacity", "1");
+          timer = setInterval(showTimer, 1000);
+        });
+        let app = {
+            cards: [1,1,2,2,3,3,4,4,5,5,6,6],
+            init: function() {
+                app.shuffle();
+                app.assignCards();
+            },
+            shuffle: function() {
+                let random = 0;
+                let temp = 0;
+                for(let i = 1; i < app.cards.length; i++) {
+                    random = Math.round(Math.random() * i);
+                    temp = app.cards[i];
+                    app.cards[i] = app.cards[random];
+                    app.cards[random] = temp;
+                }
+                app.assignCards();
+                console.log(app.cards);
+            },
+            assignCards: function() {
+                $('.card').each(function(index) {
+                    $(this).attr(`data-card-value`, app.cards[index]);
+                })
+                app.clickHandlers();
+            },
+            clickHandlers: function() {
+                $('.card').on('click', function() {
+                    $(this).html(`<p>`+$(this).data('cardValue')+`</p`).addClass(`selected`);
+                    app.checkMatch();
+                });
+            },
+            checkMatch: function() {
+                if($('.selected').length === 2) {
+                    if ($(`.selected`).first().data(`cardValue`) == $(`.selected`).last().data(`cardValue`)) {
+                        $('.selected').each(function() {
+                            $(this).animate({opacity: 0}).removeClass(`unmatched`);
+                        });
+                        $(`.selected`).each(function() {
+                            $(this).removeClass('selected');
+                        });
+                        app.checkWin();
+                    } else {
+                        setTimeout(function(){
+                            $(`.selected`).each(function() {
+                            $(this).html(``).removeClass('selected');
+                            });
+                        }, 1000);
+                    }
+    
+                }
+            },
+            checkWin: function() {
+                if($(`.unmatched`).length === 0) {
+                    $(`.container`).html(`<h1>you won!<h1>`);
+                }
+            }
+        };
+        app.init()
+    function showTimer() {
+          if (time < 0) {
+            clearInterval(timer);
+                return;
+          }
+          function pad(value) {
+                return (value < 10 ? `0` : ``) + value;
+          }
+          $(`.timer`).text(Math.floor(time / 60) + `:` + pad(time % 60));
+          time--;
+        }
+ })
+$(document).ready(function() {
+    // let app = {
+    //     cards: [1,1,2,2,3,3,4,4,5,5,6,6],
+    //     init: function() {
+    //         app.shuffle();
+    //         app.assignCards();
+    //     },
+    //     shuffle: function() {
+    //         let random = 0;
+    //         let temp = 0;
+    //         for(let i = 1; i < app.cards.length; i++) {
+    //             random = Math.round(Math.random() * i);
+    //             temp = app.cards[i];
+    //             app.cards[i] = app.cards[random];
+    //             app.cards[random] = temp;
+    //         }
+    //         app.assignCards();
+    //         console.log(app.cards);
+    //     },
+    //     assignCards: function() {
+    //         $('.card').each(function(index) {
+    //             $(this).attr(`data-card-value`, app.cards[index]);
+    //         })
+    //         app.clickHandlers();
+    //     },
+    //     clickHandlers: function() {
+    //         $('.card').on('click', function() {
+    //             $(this).html(`<p>`+$(this).data('cardValue')+`</p`).addClass(`selected`);
+    //             app.checkMatch();
+    //         });
+    //     },
+    //     checkMatch: function() {
+    //         if($('.selected').length === 2) {
+    //             if ($(`.selected`).first().data(`cardValue`) == $(`.selected`).last().data(`cardValue`)) {
+    //                 $('.selected').each(function() {
+    //                     $(this).animate({opacity: 0}).removeClass(`unmatched`);
+    //                 });
+    //                 $(`.selected`).each(function() {
+    //                     $(this).removeClass('selected');
+    //                 });
+    //                 app.checkWin();
+    //             } else {
+    //                 setTimeout(function(){
+    //                     $(`.selected`).each(function() {
+    //                     $(this).html(``).removeClass('selected');
+    //                     });
+    //                 }, 1000);
+    //             }
 
-
-
-//     let clicked = true
-//     $(".power").click(function() {
-//         console.log("clicked")
-//         $(".screen").css("background", "url(http://www.sprinklesandbooze.com/wp/wp-content/uploads/2012/11/pete-and-pete.jpeg)");
-//         let clicked = false;
-//         console.log(clicked);
-//     })
-//     if ($(window).width() < 820 && clicked == false) {
-//         $(".power").click(function() {
-//         console.log('working')
-//         $(".screen").css("background-color", "#000000");
-//         })
-
-//     // let image = clicked ? "rgb(83, 104, 46)": "url(http://www.sprinklesandbooze.com/wp/wp-content/uploads/2012/11/pete-and-pete.jpeg)";
-//     // $(".screen").css(image);
-//     // clicked = !clicked;
-//     // console.log('hello')
-// }
+    //         }
+    //     },
+    //     checkWin: function() {
+    //         if($(`.unmatched`).length === 0) {
+    //             $(`.container`).html(`<h1>you won!<h1>`);
+    //         }
+    //     }
+    // };
+    // app.init()
+})
